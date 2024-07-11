6675beb18cd015ce3bc9a8a0d6894b2a0d02c704
roro11fr <franciuc455@gmail.com>
2024-07-11 15:37:15 +0300

    rezolvare hover tobbar si implementare pagina about

---
+++ Celestin.API/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Celestin.API.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
